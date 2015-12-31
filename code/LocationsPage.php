61afe53428e042d8d8bb14b2b60fb075fb241a30
helpfulrobot <chris@silverstripe.com>
2016-01-01 01:40:14 +1300

    Converted to PSR-2

--- code/LocationsPage.php
+++ code/LocationsPage.php
@@ -1,7 +1,7 @@
-<?
+<?php
 	class Location extends DataObject
 	{
-		private static $db = array( 
+	    private static $db = array( 
 			"SortOrder" => "Int",
 			"Title" => "Varchar(255)",
 			"Address" => "Varchar(255)",
@@ -9,88 +9,103 @@
 			"MapLongitude" => "Varchar(255)"
 		);
 		
-		private static $default_sort = "SortOrder";
+	    private static $default_sort = "SortOrder";
 		
-		private static $summary_fields = array(
+	    private static $summary_fields = array(
 			"Title" => "Title",
 			"Address" => "Address"
 		);
 		
-		private static $has_one = array(
+	    private static $has_one = array(
 			"LocationsPage" => "LocationsPage"
 		); 		
 		
-        public function getCMSFields()
-        {
-			return new FieldList(
+	    public function getCMSFields()
+	    {
+	        return new FieldList(
 				new TextField("Title", "Location Title"),
 				new TextField("Address", "Location Address")
 			);
-        }
+	    }
 		
-		public function getLocation($address=false){
-			$google = "http://maps.google.com/maps/api/geocode/json?sensor=false&address=";
-			$url = $google.urlencode($address);
+	    public function getLocation($address=false)
+	    {
+	        $google = "http://maps.google.com/maps/api/geocode/json?sensor=false&address=";
+	        $url = $google.urlencode($address);
 			
-			$resp_json = $this->curl_file_get_contents($url);
-			$resp = json_decode($resp_json, true);
+	        $resp_json = $this->curl_file_get_contents($url);
+	        $resp = json_decode($resp_json, true);
 	
-			if($resp['status']='OK'){
-				return $resp['results'][0]['geometry']['location'];
-			}else{
-				return false;
-			}
-		}
+	        if ($resp['status']='OK') {
+	            return $resp['results'][0]['geometry']['location'];
+	        } else {
+	            return false;
+	        }
+	    }
 		
-		private function curl_file_get_contents($URL)
-		{
-			$c = curl_init();
-			curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
-			curl_setopt($c, CURLOPT_URL, $URL);
-			$contents = curl_exec($c);
-			curl_close($c);
+	    private function curl_file_get_contents($URL)
+	    {
+	        $c = curl_init();
+	        curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
+	        curl_setopt($c, CURLOPT_URL, $URL);
+	        $contents = curl_exec($c);
+	        curl_close($c);
 	
-			if ($contents) return $contents;
-				else return FALSE;
-		}
+	        if ($contents) {
+	            return $contents;
+	        } else {
+	            return false;
+	        }
+	    }
 		
-		function onAfterWrite()
-		{
-			parent::onAfterWrite();
+	    public function onAfterWrite()
+	    {
+	        parent::onAfterWrite();
 			
-			$location = $this->getLocation($this->Address);
-			if ($location)
-			{
-				$this->MapLatitude = $location['lat'];
-				$this->MapLongitude = $location['lng'];
-				$this->write();
-			}
-		}
+	        $location = $this->getLocation($this->Address);
+	        if ($location) {
+	            $this->MapLatitude = $location['lat'];
+	            $this->MapLongitude = $location['lng'];
+	            $this->write();
+	        }
+	    }
 		
-		public function canCreate($member = null) { return true; }
-		public function canDelete($member = null) { return true; }
-		public function canEdit($member = null)   { return true; }
-		public function canView($member = null)   { return true; }
+	    public function canCreate($member = null)
+	    {
+	        return true;
+	    }
+	    public function canDelete($member = null)
+	    {
+	        return true;
+	    }
+	    public function canEdit($member = null)
+	    {
+	        return true;
+	    }
+	    public function canView($member = null)
+	    {
+	        return true;
+	    }
 	}
 	
 	class LocationsPage extends Page
 	{
-		private static $icon = "themes/mysite/images/icons/icon-locations";
+	    private static $icon = "themes/mysite/images/icons/icon-locations";
 		
-		private static $db = array(
+	    private static $db = array(
 			"MapType" => "Varchar(255)",
 			"MapDirections" => "Boolean"
 		);
 		
-		private static $has_many = array(
+	    private static $has_many = array(
 			"Locations" => "Location"
 		);
 		
-		public function getCMSFields()
-		{
-			$fields = parent::getCMSFields();
-			$fields->addFieldToTab("Root.Content.MapDetails", new DropdownField("MapType", "Map Display Type", array("ROADMAP"=>"Roadmap","SATELLITE"=>"Satellite","HYBRID"=>"Hybrid","TERRAIN"=>"Terrain"),"Roadmap"));
-			$locations_config = GridFieldConfig::create()->addComponents(				
+	    public function getCMSFields()
+	    {
+	        $fields = parent::getCMSFields();
+	        $fields->addFieldToTab("Root.Content.MapDetails", new DropdownField("MapType", "Map Display Type", array("ROADMAP"=>"Roadmap", "SATELLITE"=>"Satellite", "HYBRID"=>"Hybrid", "TERRAIN"=>"Terrain"), "Roadmap"));
+	        $locations_config = GridFieldConfig::create()->addComponents(				
 				new GridFieldSortableRows('SortOrder'),
 				new GridFieldToolbarHeader(),
 				new GridFieldAddNewButton('toolbar-header-right'),
@@ -101,89 +116,91 @@
 				new GridFieldDeleteAction(),
 				new GridFieldDetailForm()				
 			);
-			$fields->addFieldToTab('Root.Content.MapDetails', new GridField('Locations','Locations',$this->Locations(),$locations_config));
-			$fields->addFieldToTab("Root.Content.MapDetails", new CheckboxField("MapDirections", "Display Directions Widget?"));
-			return $fields;
-		}			
+	        $fields->addFieldToTab('Root.Content.MapDetails', new GridField('Locations', 'Locations', $this->Locations(), $locations_config));
+	        $fields->addFieldToTab("Root.Content.MapDetails", new CheckboxField("MapDirections", "Display Directions Widget?"));
+	        return $fields;
+	    }
 	}
 	
 	class LocationsPage_Controller extends Page_Controller
 	{
 		
-		private static $allowed_actions = array(
+	    private static $allowed_actions = array(
 			"directions",
 			"printview"			
 		);	
 		
-		public function init()
-		{
-			Requirements::javascript("http://maps.googleapis.com/maps/api/js?key=AIzaSyAXy4BLGXyLMakRQbrMVrFxS2KiXSj51cM&sensor=false");
-			parent::init();
-		}
+	    public function init()
+	    {
+	        Requirements::javascript("http://maps.googleapis.com/maps/api/js?key=AIzaSyAXy4BLGXyLMakRQbrMVrFxS2KiXSj51cM&sensor=false");
+	        parent::init();
+	    }
 		
-		function PageCSS()
-		{
-			$dir = ViewableData::ThemeDir();
-			return array_merge(
+	    public function PageCSS()
+	    {
+	        $dir = ViewableData::ThemeDir();
+	        return array_merge(
 				parent::PageCSS(),
 				array(
 					$dir."/css/form.css"
 				)
 			);
-		}
+	    }
 		
-		function CustomJS()
-		{
-			$js = parent::CustomJS();
-			$js .= 'var MapType = "'.$this->MapType.'";
+	    public function CustomJS()
+	    {
+	        $js = parent::CustomJS();
+	        $js .= 'var MapType = "'.$this->MapType.'";
 					var address_objects = [];';
-			if($locations = $this->Locations()){
-				foreach($locations as $key => $l){
-					$js .= 'address_objects['.$key.'] = {"Title":"'.$l->Title.'","Address":"'.$l->Address.'","LatLng":['.$l->MapLatitude.','.$l->MapLongitude.']};';
-				}
-			}
-			$js .= 'var Avgs = '.$this->Avgs().';
+	        if ($locations = $this->Locations()) {
+	            foreach ($locations as $key => $l) {
+	                $js .= 'address_objects['.$key.'] = {"Title":"'.$l->Title.'","Address":"'.$l->Address.'","LatLng":['.$l->MapLatitude.','.$l->MapLongitude.']};';
+	            }
+	        }
+	        $js .= 'var Avgs = '.$this->Avgs().';
 					var PageLink = "'.$this->Link().'";';
 			
 			
-			return $js;
-		}
+	        return $js;
+	    }
 		
-		public function Avgs(){			
-			$TotalLat = 0;
-			$TotalLong = 0;
-			$Total = 0;
-			if($locations = $this->Locations())
-			{
-				foreach($locations as $l){
-					$TotalLat += $l->MapLatitude;
-					$TotalLong += $l->MapLongitude;
-					$Total++;
-				}
-				if($Total)return "[".$TotalLat/$Total.",".$TotalLong/$Total."]";
-			}
-			return false;
-		}
+	    public function Avgs()
+	    {
+	        $TotalLat = 0;
+	        $TotalLong = 0;
+	        $Total = 0;
+	        if ($locations = $this->Locations()) {
+	            foreach ($locations as $l) {
+	                $TotalLat += $l->MapLatitude;
+	                $TotalLong += $l->MapLongitude;
+	                $Total++;
+	            }
+	            if ($Total) {
+	                return "[".$TotalLat/$Total.",".$TotalLong/$Total."]";
+	            }
+	        }
+	        return false;
+	    }
 		
-		public function directionsAPI()
-		{
-			$to_addy = urlencode($this->request->param('ID'));
-			$from_addy = urlencode($this->request->param('OtherID'));
-			$path = "http://maps.googleapis.com/maps/api/directions/json?origin=".$from_addy."&destination=".$to_addy."&sensor=false";
-			$rows = file_get_contents($path,0,null,null);
-			$directions_output = json_decode($rows, true);
-			$ajax_data = false;
+	    public function directionsAPI()
+	    {
+	        $to_addy = urlencode($this->request->param('ID'));
+	        $from_addy = urlencode($this->request->param('OtherID'));
+	        $path = "http://maps.googleapis.com/maps/api/directions/json?origin=".$from_addy."&destination=".$to_addy."&sensor=false";
+	        $rows = file_get_contents($path, 0, null, null);
+	        $directions_output = json_decode($rows, true);
+	        $ajax_data = false;
 			
-			if($directions_output['routes']){
-				$data = $directions_output['routes'][0]['legs'][0];  //assumes best route and no waypoints
+	        if ($directions_output['routes']) {
+	            $data = $directions_output['routes'][0]['legs'][0];  //assumes best route and no waypoints
 				$i = 1;
-				$steps = "";
-				foreach($data['steps'] as $step){
-					$steps .= "<div class='step'><span class='step_number'>".$i.".</span><span class='step_text'>".$step['html_instructions']."</span><span class='step_distance'>".$step['distance']['text']."</span></div>";	
-					$i++;
-				}
+	            $steps = "";
+	            foreach ($data['steps'] as $step) {
+	                $steps .= "<div class='step'><span class='step_number'>".$i.".</span><span class='step_text'>".$step['html_instructions']."</span><span class='step_distance'>".$step['distance']['text']."</span></div>";	
+	                $i++;
+	            }
 				
-				$ajax_data = array(
+	            $ajax_data = array(
 					"StartAddress" => $data['start_address'],
 					"EndAddress" => $data['end_address'],
 					"Distance" => $data['distance']['text'],
@@ -193,35 +210,35 @@
 					"PageLink" => $this->AbsoluteLink()."directions/".$from_addy."/".$to_addy,
 					"Steps" => $steps
 				);
-				
-			}
+	        }
 			
-			return $ajax_data;
-		}
+	        return $ajax_data;
+	    }
 		
-		public function directions()
-		{
-			$ajax_data = $this->directionsAPI();
+	    public function directions()
+	    {
+	        $ajax_data = $this->directionsAPI();
 			
-			if($ajax_data){
-				return Director::is_ajax() ? $this->Customise($ajax_data)->renderWith("ContactPage_directions") : $this->Customise($ajax_data);
-			} else {
-				return "<p>No routes were found from that destination address.</p>";	
-			}
-		}
+	        if ($ajax_data) {
+	            return Director::is_ajax() ? $this->Customise($ajax_data)->renderWith("ContactPage_directions") : $this->Customise($ajax_data);
+	        } else {
+	            return "<p>No routes were found from that destination address.</p>";
+	        }
+	    }
 		
-		public function printview()
-		{
-			$ajax_data = $this->directionsAPI();
+	    public function printview()
+	    {
+	        $ajax_data = $this->directionsAPI();
 			
-			if($ajax_data){
-				return $this->Customise($ajax_data)->renderWith("ContactPage_printview");
-			} else {
-				return "<p>No routes were found from that destination address.</p>";	
-			}
-		}
+	        if ($ajax_data) {
+	            return $this->Customise($ajax_data)->renderWith("ContactPage_printview");
+	        } else {
+	            return "<p>No routes were found from that destination address.</p>";
+	        }
+	    }
 		
-		public function NeedLocationsSelect(){
-			return $this->Locations()->Count() > 1;	
-		}
+	    public function NeedLocationsSelect()
+	    {
+	        return $this->Locations()->Count() > 1;
+	    }
 	}
